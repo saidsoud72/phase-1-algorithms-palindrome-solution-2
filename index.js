98686905562b433a1ddf6f061c0e2135b7ebeee3
saidsoud72 <said.omar@student.moringaschool.com>
2022-08-30 11:52:13 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,25 +1,41 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+	for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+		const endIndex = word.length - 1 - startIndex;
+		if (word[startIndex] !== word[endIndex]) {
+			return false;
+		}
+	}
+
+	return true;
 }
 
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+if we reach the middle, and all the letters match, return true
 */
 
 /*
   Add written explanation of your solution here
+  We iterate from the beginning of the string to the middle of the string.
+  We compare the letter we're iterating over to the corresponding letter at the end of the string.
+  If the letters don't match, return false.
+  If we reach the middle, and all the letters match, return true.
+  
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+	// add your own custom tests in here
+	console.log("Expecting: true");
+	console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+	console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+	console.log("Expecting: false");
+	console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
